f21abff45f19e752afa19b3f2300aa5e03340f43
kennedy <kimaniken2001@gmail.com>
2023-05-22 10:11:30 -0400

    done

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,16 +6,39 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      raise PartnerError, "Invalid argument. Must be a Person instance."
+    end
   end
 
+  class PartnerError < StandardError
+    def message
+      "Invalid argument. Must be a Person instance."
+    end
+  end
 end
 
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
+describe Person do
+  let(:beyonce) { Person.new("Beyonce") }
+  let(:jay_z) { Person.new("Jay-Z") }
 
+  describe "#get_married" do
+    context "when passed a Person instance as an argument" do
+      it "sets the partners of each person" do
+        beyonce.get_married(jay_z)
+        expect(beyonce.partner).to be(jay_z)
+        expect(jay_z.partner).to be(beyonce)
+      end
+    end
 
-
-
+    context "when passed something other than a Person instance as an argument" do
+      it "outputs an error message to the terminal" do
+        error_message = "Invalid argument. Must be a Person instance."
+        expect { beyonce.get_married("Jay-Z") }.to raise_error(Person::PartnerError, error_message)
+      end
+    end
+  end
+end
